dd2285abb84b79642ed9553af4a36cfb795d5ebe
Aditya Singh Tomar <adityastomar12@gmail.com>
2023-03-10 14:43:22 +0530

    plugin settings update

--- lua/plugins/ui/italicize.lua
+++ lua/plugins/ui/italicize.lua
@@ -2,8 +2,19 @@ return {
     "adityastomar67/italicize",
     enabled = vim.g.plugin_enabled.italicize,
     opts = {
-        transparency = true,
-        italics      = true,
+        transparency       = true,
+        italics            = true,
+        transparent_groups = {
+            "Todo",
+            "TodoSignDONE",
+            "TodoSignFIX ",
+            "TodoSignHACK",
+            "TodoSignNOTE",
+            "TodoSignPERF",
+            "TodoSignTEST",
+            "TodoSignTODO",
+            "TodoSignWARN",
+        }
     },
     config = function(_, opts)
         require("italicize").setup(opts)
