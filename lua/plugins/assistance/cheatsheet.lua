029c06453e1c21e96bd3651fbd442626e2e06398
Aditya Singh Tomar <adityastomar12@gmail.com>
2023-03-10 12:12:09 +0530

    Assistance tool update

--- lua/plugins/assistance/cheatsheet.lua
+++ lua/plugins/assistance/cheatsheet.lua
@@ -2,16 +2,17 @@ return {
     "sudormrfbin/cheatsheet.nvim",
     enabled = vim.g.plugin_enabled.cheatsheet,
     event = "VeryLazy",
-    opts = {
-        bundled_cheatsheets = true,
-        bundled_plugin_cheatsheets = true,
-        include_only_installed_plugins = true,
-        telescope_mappings = {
-            ["<CR>"] = require("cheatsheet.telescope.actions").select_or_fill_commandline,
-            ["<A-CR>"] = require("cheatsheet.telescope.actions").select_or_execute,
-            ["<C-Y>"] = require("cheatsheet.telescope.actions").copy_cheat_value,
-            ["<C-E>"] = require("cheatsheet.telescope.actions").edit_user_cheatsheet
-        }
-    },
-    config = function(_, opts) require("cheatsheet").setup(opts) end
+    config = function()
+        require("cheatsheet").setup({
+            bundled_cheatsheets = true,
+            bundled_plugin_cheatsheets = true,
+            include_only_installed_plugins = true,
+            telescope_mappings = {
+                ["<CR>"] = require("cheatsheet.telescope.actions").select_or_fill_commandline,
+                ["<A-CR>"] = require("cheatsheet.telescope.actions").select_or_execute,
+                ["<C-Y>"] = require("cheatsheet.telescope.actions").copy_cheat_value,
+                ["<C-E>"] = require("cheatsheet.telescope.actions").edit_user_cheatsheet
+            }
+        })
+    end
 }
