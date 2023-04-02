5edb3cff07c34995873e24bb0d77be529052fb19
Aditya Singh Tomar <adityastomar12@gmail.com>
2023-04-02 11:22:46 +0530

    bugs

--- lua/plugins/lsp/servers/setup.lua
+++ lua/plugins/lsp/servers/setup.lua
@@ -171,36 +171,6 @@ require("lspconfig").lua_ls.setup {
         },
     },
 }
-require("lspconfig").clangd.setup({
-    on_attach = M.on_attach,
-    capabilities = M.capabilities,
-    cmd = {
-        "clangd",
-        "--background-index",
-        "--pch-storage=memory",
-        "--clang-tidy",
-        "--suggest-missing-includes",
-        "--cross-file-rename",
-        "--completion-style=detailed",
-    },
-    init_options = {
-        clangdFileStatus     = true,
-        usePlaceholders      = true,
-        completeUnimported   = true,
-        semanticHighlighting = true,
-    },
-    filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
-    log_level = 2,
-    root_dir = require("lspconfig.util").root_pattern({
-            ".clangd",
-            ".clang-tidy",
-            ".clang-format",
-            "compile_commands.json",
-            "compile_flags.txt",
-            "configure.ac",
-            ".git",
-        }) or vim.loop.cwd(),
-    single_file_support = true,
-})
+require("lspconfig").clangd.setup(require("plugins.lsp.servers.clangd"))
 
 return M
