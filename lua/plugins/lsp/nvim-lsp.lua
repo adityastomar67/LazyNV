70ad2398f0ef68de20f97ba5a23240c698bd44ed
Aditya Singh Tomar <adityastomar12@gmail.com>
2023-04-02 23:03:59 +0530

    typescript

--- lua/plugins/lsp/nvim-lsp.lua
+++ lua/plugins/lsp/nvim-lsp.lua
@@ -27,33 +27,50 @@ local lazy_load = function(plugin)
 end
 
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-nvim-lsp-signature-help",
+    {
         "jose-elias-alvarez/typescript.nvim",
-        "b0o/SchemaStore.nvim",
-    },
-    init = lazy_load "nvim-lspconfig",
-    config = function()
-        local lsp = require("lspconfig")
-        local icons = require("utils.icons").icons.diagnostics
-        vim.diagnostic.config({ virtual_text = true })
-
-        local diagnostic_signs = {
-            { name = "DiagnosticSignError", text = icons.Error },
-            { name = "DiagnosticSignWarn",  text = icons.Warning },
-            { name = "DiagnosticSignHint",  text = icons.Hint },
-            { name = "DiagnosticSignInfo",  text = icons.Information },
-        }
-
-        for _, sign in ipairs(diagnostic_signs) do
-            vim.fn.sign_define(sign.name, {
-                texthl = sign.name,
-                text   = sign.text,
-                numhl  = sign.name,
+        event = "VeryLazy",
+        config = function()
+            require("typescript").setup({
+                disable_commands = false, -- prevent the plugin from creating Vim commands
+                debug = false,        -- enable debug logging for commands
+                go_to_source_definition = {
+                    fallback = true,  -- fall back to standard LSP definition on failure
+                },
+                server = {            -- pass options to lspconfig's setup method
+                    on_attach = require("plugins.lsp.servers.setup").on_attach,
+                },
             })
-        end
-        require "plugins.lsp.servers.setup"
-    end,
-}
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-nvim-lsp-signature-help",
+            "jose-elias-alvarez/typescript.nvim",
+            "b0o/SchemaStore.nvim",
+        },
+        init = lazy_load "nvim-lspconfig",
+        config = function()
+            local lsp = require("lspconfig")
+            local icons = require("utils.icons").icons.diagnostics
+            vim.diagnostic.config({ virtual_text = true })
+
+            local diagnostic_signs = {
+                { name = "DiagnosticSignError", text = icons.Error },
+                { name = "DiagnosticSignWarn",  text = icons.Warning },
+                { name = "DiagnosticSignHint",  text = icons.Hint },
+                { name = "DiagnosticSignInfo",  text = icons.Information },
+            }
+
+            for _, sign in ipairs(diagnostic_signs) do
+                vim.fn.sign_define(sign.name, {
+                    texthl = sign.name,
+                    text   = sign.text,
+                    numhl  = sign.name,
+                })
+            end
+            require "plugins.lsp.servers.setup"
+        end,
+    } }
--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -3,6 +3,7 @@ return {
     "nvim-tree/nvim-web-devicons",
     {
         "NvChad/nvim-colorizer.lua",
+        event = "VeryLazy",
         config = function()
             require('colorizer').setup({
                 user_default_options = {
