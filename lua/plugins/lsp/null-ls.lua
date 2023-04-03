d664c83873e9afc31a7ebca64041451273a0248d
Aditya Singh Tomar <adityastomar12@gmail.com>
2023-04-03 08:44:06 +0530

    updates

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -6,19 +6,19 @@ return {
         local nls = require "null-ls"
         local b = nls.builtins
         local sources = {
-            b.formatting.deno_fmt, 
+            b.formatting.deno_fmt,
             b.formatting.prettier.with({
-            filetypes = { "html", "markdown", "css" },
-            extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" }
-            }),                                                              -- Lua
-            b.formatting.stylua,                                         -- Shell
+                filetypes = { "html", "markdown", "css" },
+                extra_args = { "--indent-width=4", "--single-quote", "--jsx-single-quote" }
+            }),
+            b.formatting.stylua,
             b.formatting.shfmt,
-            b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }), -- cpp
-            b.formatting.clang_format,                                   -- pyhton
-            b.formatting.black.with({ extra_args = { "--fast" } }), 
+            b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
+            b.formatting.clang_format,
+            b.formatting.black.with({ extra_args = { "--fast" } }),
             b.diagnostics.flake8,
             b.diagnostics.ruff.with { extra_args = { "--max-line-length=180" } }
-            }
+        }
         nls.setup({ debug = true, sources = sources })
     end
 }
--- lua/plugins/lsp/servers/setup.lua
+++ lua/plugins/lsp/servers/setup.lua
@@ -1,5 +1,136 @@
 local M = {}
+local merge_tb = vim.tbl_deep_extend
 -- local utils = require "core.utils"
+local lspconfig = {
+    plugin = true,
+    -- See `<cmd> :help vim.lsp.*` for documentation on any of the below functions
+
+    n = {
+        ["gD"] = {
+            function()
+                vim.lsp.buf.declaration()
+            end,
+            "lsp declaration",
+        },
+        ["gd"] = {
+            function()
+                vim.lsp.buf.definition()
+            end,
+            "lsp definition",
+        },
+        ["K"] = {
+            function()
+                vim.lsp.buf.hover()
+            end,
+            "lsp hover",
+        },
+        ["gi"] = {
+            function()
+                vim.lsp.buf.implementation()
+            end,
+            "lsp implementation",
+        },
+        ["<leader>ls"] = {
+            function()
+                vim.lsp.buf.signature_help()
+            end,
+            "lsp signature_help",
+        },
+        ["<leader>D"] = {
+            function()
+                vim.lsp.buf.type_definition()
+            end,
+            "lsp definition type",
+        },
+        ["<leader>ra"] = {
+            function()
+                require("nvchad_ui.renamer").open()
+            end,
+            "lsp rename",
+        },
+        ["<leader>ca"] = {
+            function()
+                vim.lsp.buf.code_action()
+            end,
+            "lsp code_action",
+        },
+        ["gr"] = {
+            function()
+                vim.lsp.buf.references()
+            end,
+            "lsp references",
+        },
+        ["<leader>f"] = {
+            function()
+                vim.diagnostic.open_float { border = "rounded" }
+            end,
+            "floating diagnostic",
+        },
+        ["[d"] = {
+            function()
+                vim.diagnostic.goto_prev()
+            end,
+            "goto prev",
+        },
+        ["]d"] = {
+            function()
+                vim.diagnostic.goto_next()
+            end,
+            "goto_next",
+        },
+        ["<leader>q"] = {
+            function()
+                vim.diagnostic.setloclist()
+            end,
+            "diagnostic setloclist",
+        },
+        ["<leader>fm"] = {
+            function()
+                vim.lsp.buf.format { async = true }
+            end,
+            "lsp formatting",
+        },
+        ["<leader>wa"] = {
+            function()
+                vim.lsp.buf.add_workspace_folder()
+            end,
+            "add workspace folder",
+        },
+        ["<leader>wr"] = {
+            function()
+                vim.lsp.buf.remove_workspace_folder()
+            end,
+            "remove workspace folder",
+        },
+        ["<leader>wl"] = {
+            function()
+                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            end,
+            "list workspace folders",
+        },
+    },
+}
+
+local function set_section_map(section_values)
+    if section_values.plugin then
+        return
+    end
+
+    section_values.plugin = nil
+
+    for mode, mode_values in pairs(section_values) do
+        local default_opts = merge_tb("force", { mode = mode }, { buffer = bufnr } or {})
+        for keybind, mapping_info in pairs(mode_values) do
+            -- merge default + user opts
+            local opts = merge_tb("force", default_opts, mapping_info.opts or {})
+
+            mapping_info.opts, opts.mode = nil, nil
+            opts.desc = mapping_info[2]
+
+            vim.keymap.set(mode, keybind, mapping_info[1], opts)
+        end
+    end
+end
 
 M.signature_window = function(_, result, ctx, config)
     local bufnr, winner = vim.lsp.handlers.signature_help(_, result, ctx, config)
@@ -109,6 +240,7 @@ M.on_attach = function(client, bufnr)
     client.server_capabilities.documentRangeFormattingProvider = false
 
     -- utils.load_mappings("lspconfig", { buffer = bufnr })
+    set_section_map(lspconfig)
 
     if client.server_capabilities.signatureHelpProvider then
         M.setup(client)
@@ -171,8 +303,8 @@ require("lspconfig").clangd.setup(require("plugins.lsp.servers.clangd"))
 require("lspconfig").bashls.setup(require("plugins.lsp.servers.bashls"))
 require("lspconfig").tsserver.setup({
     on_attach = M.on_attach,
-    filetypes = {"typescript", "typescriptreact", "typescript.jsx", "javascriptreact"},
-    cmd = {"typescript-language-server", "--stdio"}
+    filetypes = { "typescript", "typescriptreact", "typescript.jsx", "javascriptreact" },
+    cmd = { "typescript-language-server", "--stdio" }
 })
 require("lspconfig").tailwindcss.setup({
     on_attach = M.on_attach,
