640462f7ff1ab34628a19954560feb17b478a0a4
Aditya Singh Tomar <adityastomar12@gmail.com>
2023-04-03 12:51:32 +0530

    Update setup.lua

--- lua/plugins/lsp/servers/setup.lua
+++ lua/plugins/lsp/servers/setup.lua
@@ -1,136 +1,136 @@
 local M = {}
 local merge_tb = vim.tbl_deep_extend
--- local utils = require "core.utils"
-local lspconfig = {
-    plugin = true,
-    -- See `<cmd> :help vim.lsp.*` for documentation on any of the below functions
+-- -- local utils = require "core.utils"
+-- local lspconfig = {
+--     plugin = true,
+--     -- See `<cmd> :help vim.lsp.*` for documentation on any of the below functions
 
-    n = {
-        ["gD"] = {
-            function()
-                vim.lsp.buf.declaration()
-            end,
-            "lsp declaration",
-        },
-        ["gd"] = {
-            function()
-                vim.lsp.buf.definition()
-            end,
-            "lsp definition",
-        },
-        ["K"] = {
-            function()
-                vim.lsp.buf.hover()
-            end,
-            "lsp hover",
-        },
-        ["gi"] = {
-            function()
-                vim.lsp.buf.implementation()
-            end,
-            "lsp implementation",
-        },
-        ["<leader>ls"] = {
-            function()
-                vim.lsp.buf.signature_help()
-            end,
-            "lsp signature_help",
-        },
-        ["<leader>D"] = {
-            function()
-                vim.lsp.buf.type_definition()
-            end,
-            "lsp definition type",
-        },
-        ["<leader>ra"] = {
-            function()
-                require("nvchad_ui.renamer").open()
-            end,
-            "lsp rename",
-        },
-        ["<leader>ca"] = {
-            function()
-                vim.lsp.buf.code_action()
-            end,
-            "lsp code_action",
-        },
-        ["gr"] = {
-            function()
-                vim.lsp.buf.references()
-            end,
-            "lsp references",
-        },
-        ["<leader>f"] = {
-            function()
-                vim.diagnostic.open_float { border = "rounded" }
-            end,
-            "floating diagnostic",
-        },
-        ["[d"] = {
-            function()
-                vim.diagnostic.goto_prev()
-            end,
-            "goto prev",
-        },
-        ["]d"] = {
-            function()
-                vim.diagnostic.goto_next()
-            end,
-            "goto_next",
-        },
-        ["<leader>q"] = {
-            function()
-                vim.diagnostic.setloclist()
-            end,
-            "diagnostic setloclist",
-        },
-        ["<leader>fm"] = {
-            function()
-                vim.lsp.buf.format { async = true }
-            end,
-            "lsp formatting",
-        },
-        ["<leader>wa"] = {
-            function()
-                vim.lsp.buf.add_workspace_folder()
-            end,
-            "add workspace folder",
-        },
-        ["<leader>wr"] = {
-            function()
-                vim.lsp.buf.remove_workspace_folder()
-            end,
-            "remove workspace folder",
-        },
-        ["<leader>wl"] = {
-            function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end,
-            "list workspace folders",
-        },
-    },
-}
+--     n = {
+--         ["gD"] = {
+--             function()
+--                 vim.lsp.buf.declaration()
+--             end,
+--             "lsp declaration",
+--         },
+--         ["gd"] = {
+--             function()
+--                 vim.lsp.buf.definition()
+--             end,
+--             "lsp definition",
+--         },
+--         ["K"] = {
+--             function()
+--                 vim.lsp.buf.hover()
+--             end,
+--             "lsp hover",
+--         },
+--         ["gi"] = {
+--             function()
+--                 vim.lsp.buf.implementation()
+--             end,
+--             "lsp implementation",
+--         },
+--         ["<leader>ls"] = {
+--             function()
+--                 vim.lsp.buf.signature_help()
+--             end,
+--             "lsp signature_help",
+--         },
+--         ["<leader>D"] = {
+--             function()
+--                 vim.lsp.buf.type_definition()
+--             end,
+--             "lsp definition type",
+--         },
+--         ["<leader>ra"] = {
+--             function()
+--                 require("nvchad_ui.renamer").open()
+--             end,
+--             "lsp rename",
+--         },
+--         ["<leader>ca"] = {
+--             function()
+--                 vim.lsp.buf.code_action()
+--             end,
+--             "lsp code_action",
+--         },
+--         ["gr"] = {
+--             function()
+--                 vim.lsp.buf.references()
+--             end,
+--             "lsp references",
+--         },
+--         ["<leader>f"] = {
+--             function()
+--                 vim.diagnostic.open_float { border = "rounded" }
+--             end,
+--             "floating diagnostic",
+--         },
+--         ["[d"] = {
+--             function()
+--                 vim.diagnostic.goto_prev()
+--             end,
+--             "goto prev",
+--         },
+--         ["]d"] = {
+--             function()
+--                 vim.diagnostic.goto_next()
+--             end,
+--             "goto_next",
+--         },
+--         ["<leader>q"] = {
+--             function()
+--                 vim.diagnostic.setloclist()
+--             end,
+--             "diagnostic setloclist",
+--         },
+--         ["<leader>fm"] = {
+--             function()
+--                 vim.lsp.buf.format { async = true }
+--             end,
+--             "lsp formatting",
+--         },
+--         ["<leader>wa"] = {
+--             function()
+--                 vim.lsp.buf.add_workspace_folder()
+--             end,
+--             "add workspace folder",
+--         },
+--         ["<leader>wr"] = {
+--             function()
+--                 vim.lsp.buf.remove_workspace_folder()
+--             end,
+--             "remove workspace folder",
+--         },
+--         ["<leader>wl"] = {
+--             function()
+--                 print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+--             end,
+--             "list workspace folders",
+--         },
+--     },
+-- }
 
-local function set_section_map(section_values)
-    if section_values.plugin then
-        return
-    end
+-- local function set_section_map(section_values)
+--     if section_values.plugin then
+--         return
+--     end
 
-    section_values.plugin = nil
+--     section_values.plugin = nil
 
-    for mode, mode_values in pairs(section_values) do
-        local default_opts = merge_tb("force", { mode = mode }, { buffer = bufnr } or {})
-        for keybind, mapping_info in pairs(mode_values) do
-            -- merge default + user opts
-            local opts = merge_tb("force", default_opts, mapping_info.opts or {})
+--     for mode, mode_values in pairs(section_values) do
+--         local default_opts = merge_tb("force", { mode = mode }, { buffer = bufnr } or {})
+--         for keybind, mapping_info in pairs(mode_values) do
+--             -- merge default + user opts
+--             local opts = merge_tb("force", default_opts, mapping_info.opts or {})
 
-            mapping_info.opts, opts.mode = nil, nil
-            opts.desc = mapping_info[2]
+--             mapping_info.opts, opts.mode = nil, nil
+--             opts.desc = mapping_info[2]
 
-            vim.keymap.set(mode, keybind, mapping_info[1], opts)
-        end
-    end
-end
+--             vim.keymap.set(mode, keybind, mapping_info[1], opts)
+--         end
+--     end
+-- end
 
 M.signature_window = function(_, result, ctx, config)
     local bufnr, winner = vim.lsp.handlers.signature_help(_, result, ctx, config)
@@ -240,7 +240,7 @@ M.on_attach = function(client, bufnr)
     client.server_capabilities.documentRangeFormattingProvider = false
 
     -- utils.load_mappings("lspconfig", { buffer = bufnr })
-    set_section_map(lspconfig)
+    -- set_section_map(lspconfig)
 
     if client.server_capabilities.signatureHelpProvider then
         M.setup(client)
@@ -312,41 +312,41 @@ require("lspconfig").tailwindcss.setup({
     cmd = { "tailwindcss-language-server", "--stdio" }
 })
 
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+-- -- Global mappings.
+-- -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+-- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+-- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+-- -- Use LspAttach autocommand to only map the following keys
+-- -- after the language server attaches to the current buffer
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--     group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+--     callback = function(ev)
+--         -- Enable completion triggered by <c-x><c-o>
+--         vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<space>mf', function()
-            vim.lsp.buf.format { async = true }
-        end, opts)
-    end,
-})
+--         -- Buffer local mappings.
+--         -- See `:help vim.lsp.*` for documentation on any of the below functions
+--         local opts = { buffer = ev.buf }
+--         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+--         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+--         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+--         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+--         vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+--         vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+--         vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+--         vim.keymap.set('n', '<space>wl', function()
+--             print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+--         end, opts)
+--         vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+--         vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+--         vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+--         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+--         vim.keymap.set('n', '<space>mf', function()
+--             vim.lsp.buf.format { async = true }
+--         end, opts)
+--     end,
+-- })
 return M
