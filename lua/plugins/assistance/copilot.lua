a71633ffc18c8973f8222652d3002b03c3e79ae7
Aditya Singh Tomar <adityastomar12@gmail.com>
2023-04-06 09:10:24 +0530

    changed copilot keymap

--- lua/plugins/assistance/copilot.lua
+++ lua/plugins/assistance/copilot.lua
@@ -24,7 +24,7 @@ return {
             auto_trigger = false,
             debounce = 75,
             keymap = {
-                accept = "<S-cr>",
+                accept = "<M-Tab>",
                 accept_word = false,
                 accept_line = false,
                 next = "<M-]>",
